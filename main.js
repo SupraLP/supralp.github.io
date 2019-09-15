ce6b74986e3194cb4f2f25c15c1ac65371df52fb
tayou <tayou@gmx.net>
2019-09-15 17:06:13 +0200

    made webapps playable after button click instead of instantaneous

---
+++ main.js
@@ -0,0 +1,15 @@
+function loadWebInteractivePlayer(location) {
+	switch (location) {
+		case "ohe":
+			document.getElementById("OpenHouseExplorer").innerHTML = '<iframe src="WebGLApplications/OpenHouseExplorer/embed.html" width="482px" height="400" scrolling="no" style="border: none;">the script couldn`t be loaded</iframe>';
+			break;
+		case "Main Game":
+			document.getElementById("maingame").innerHTML = '<iframe src="webapps/phosphorus/main-game.html" width="482px" height="400" scrolling="no" style="border: none;">the script couldn`t be loaded</iframe>';
+			break;
+		case "island":
+			document.getElementById("island").innerHTML = '<iframe src="webapps/phosphorus/island.html" width="482px" height="400" scrolling="no" style="border: none;">the script couldn`t be loaded</iframe>';
+			break;
+		default:
+			alert("something went wrong. you should inform the developer unless they already know.");
+	}
+}
