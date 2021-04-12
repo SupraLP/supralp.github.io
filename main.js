0cb90033430ff7b7c37a7e5c709ecbba619bd174
Tayou <tayou@gmx.net>
2021-04-12 14:17:08 +0200

    added GalaxyUnderAttack to projects

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ function loadWebInteractivePlayer(location) {
 		case "island":
 			document.getElementById("island").innerHTML = '<iframe src="webapps/phosphorus/island.html" width="482px" height="400" scrolling="no" style="border: none;">the script couldn`t be loaded</iframe>';
 			break;
+		case "GalaxyUnderAttack":
+			document.getElementById("GalaxyUnderAttack").innerHTML = '<iframe src="https://glt.sanz-berlin.de/supra/WebGLApplications/GalaxyUnderAttack/embed.html" width="482px" height="400" scrolling="no" style="border: none;">the script couldn`t be loaded</iframe>';
+			break;
 		default:
 			alert("something went wrong. you should inform the developer unless they already know.");
 	}
